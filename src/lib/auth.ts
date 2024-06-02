a392f619c063bb0fa0e07af93493694cb21ea734
raybagas7 <raysam.gas@gmail.com>
2024-06-03 00:01:42 +0700

    feat(add auth provider): google auth
        - add google provider into next/auth options

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
+import google from 'next-auth/providers/google';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [],
+  providers: [google],
 });
